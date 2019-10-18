8e699252fdaeea8602315b3d821ccded8c4fd2d6
N1ckGreek <nikosdaridhs@gmail.com>
2019-10-18 13:25:10 +0300

    Attack mechanic prototype
    1) Added several scripts, folders, materials to the project.
    2) Added Enemy tag.
    3) Created a prototype of Attack mechanic in TestNick Scene.

---
+++ Project Soul/Assets/Scripts/_TEST/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float angle = 200, speed = 20;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.down, angle * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up, angle * Time.deltaTime);
+        }
+    }
+}
