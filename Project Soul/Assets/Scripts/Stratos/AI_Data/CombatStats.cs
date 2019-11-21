cb34d9ea6108e8714ad7969c61424bd9caff0f56
Stratos <stratoskanavitsas@gmail.com>
2019-11-21 15:03:05 +0200

    Added CombatStats, RangerBackpack component for Agents & Implemented Ranger archetype script.

---
+++ Project Soul/Assets/Scripts/Stratos/AI_Data/CombatStats.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatStats : MonoBehaviour
+{
+    /*
+     * This class provides a variety of defensive and offensive statistics that can be modified for every agent.
+     * Commented values are for testing-reminder.
+     */
+    public int healthPoints; //100
+
+    [Header("Defence")]
+    public int physical;  //87
+    public int magic_res; //30
+
+    [Header("Resistances")]
+    public int bleed; //100
+    public int poison; //100
+
+    public bool isDisabled = false;
+    public bool isSlowed = false;
+
+    public bool isAware = false;
+}
---
+++ Project Soul/Assets/Scripts/Stratos/AI_Data/RangerBackpackComponent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Holds resources for the Ranger-type Agent.
+ */
+public class RangerBackpackComponent : MonoBehaviour
+{
+    public GameObject weapon;
+    public int numWeaponSlots = 2;
+    public int numArrows;
+    public int numPotions;
+    public string weaponType = "WeaponBow";
+    public string secondaryWeaponType = "WeaponMelee";
+}
