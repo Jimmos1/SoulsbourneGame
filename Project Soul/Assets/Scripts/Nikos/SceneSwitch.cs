2d73b7029648f50e91949cbf07b6c75b02386ba2
N1ckGreek <nikosdaridhs@gmail.com>
2020-03-10 15:42:13 +0200

    Main Menu

---
+++ Project Soul/Assets/Scripts/Nikos/SceneSwitch.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    public void LoadScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
